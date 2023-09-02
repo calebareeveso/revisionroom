4a278f66fa069618db01c9a2964a6fe6af3da3df
calebareeveso <calebareeveso7@gmail.com>
2023-09-02 10:51:35 -0400

    feat: add all features

--- app/components/call/Join.jsx
+++ app/components/call/Join.jsx
@@ -1,6 +1,5 @@
 "use client"
 import { useState } from 'react';
-import Avatar from 'boring-avatars';
 import { useHMSActions } from '@100mslive/hms-video-react';
 
 import NameInput from './Join/NameInput';
@@ -41,7 +40,7 @@ const Join = () => {
         layout="fill"
       />
       <div className="bg-gray-800 rounded-lg w-11/12 md:w-1/2 lg:w-1/3 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-8 text-white shadow-lg space-y-4 flex flex-col items-center max-w-md">
-        <Avatar name={name} variant="marble" size="72" />
+        {/* <Avatar name={name} variant="marble" size="72" /> */}
         <NameInput name={name} setName={setName} />
         <RoleSelect role={role} setRole={setRole} />
         <JoinButton joinRoom={joinRoom} />
